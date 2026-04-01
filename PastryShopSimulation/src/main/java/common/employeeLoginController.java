b477014576dc60fd42decd5fb2d961066cd3577f
Nazah-projects-dump <nazahzahin2006@gmail.com>
2026-04-01 18:20:18 +0600

    specific login fxmls + controllers

---
+++ PastryShopSimulation/src/main/java/common/employeeLoginController.java
@@ -0,0 +1,14 @@
+package common;
+
+import javafx.scene.control.TextField;
+
+public class employeeLoginController
+{
+    @javafx.fxml.FXML
+    private TextField employeeLoginName;
+    @javafx.fxml.FXML
+    private TextField employeeLoginPass;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ PastryShopSimulation/src/main/java/team/nazah/customer/customerLoginController.java
@@ -0,0 +1,12 @@
+package team.nazah.customer;
+
+import javafx.scene.control.TextField;
+
+public class customerLoginController
+{
+    @javafx.fxml.FXML
+    private TextField customerLoginName;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
