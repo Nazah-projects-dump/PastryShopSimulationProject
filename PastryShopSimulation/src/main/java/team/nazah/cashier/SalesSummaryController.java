aa473dc1cb48adbe72cac6d73c48a692eb7bf8aa
Nazah-projects-dump <nazahzahin2006@gmail.com>
2026-04-03 22:13:46 +0600

    sales summary page fxml

--- PastryShopSimulation/src/main/java/team/nazah/cashier/SalesSummaryController.java
+++ PastryShopSimulation/src/main/java/team/nazah/cashier/SalesSummaryController.java
@@ -1,7 +1,20 @@
 package team.nazah.cashier;
 
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class SalesSummaryController
 {
+    @javafx.fxml.FXML
+    private TableView salesSummaryTableView;
+    @javafx.fxml.FXML
+    private TableColumn orderIdColumn;
+    @javafx.fxml.FXML
+    private TableColumn totalColumn;
+    @javafx.fxml.FXML
+    private DatePicker salesSummaryDatePicker;
+
     @javafx.fxml.FXML
     public void initialize() {
     }}
