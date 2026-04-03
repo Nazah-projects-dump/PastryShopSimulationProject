b715de68641d096ec707f17d27389ad270ae88d4
Nazah-projects-dump <nazahzahin2006@gmail.com>
2026-04-03 19:23:39 +0600

    track orders fxml

--- PastryShopSimulation/src/main/java/team/nazah/customer/TrackOrdersController.java
+++ PastryShopSimulation/src/main/java/team/nazah/customer/TrackOrdersController.java
@@ -1,7 +1,23 @@
 package team.nazah.customer;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class TrackOrdersController
 {
     @javafx.fxml.FXML
+    private Label orderDetailsLabel;
+    @javafx.fxml.FXML
+    private Label cancellationStatusLabel;
+    @javafx.fxml.FXML
+    private TextField trackOrderIdInputTextField;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void cancelOrderButtonOnAction(ActionEvent actionEvent) {
+    }
+}
