6446992e90b607e54819b1e7b8e205a218ecb705
Nazah-projects-dump <nazahzahin2006@gmail.com>
2026-04-03 19:09:37 +0600

    payment screen fxml

--- PastryShopSimulation/src/main/java/team/nazah/customer/PaymentScreenController.java
+++ PastryShopSimulation/src/main/java/team/nazah/customer/PaymentScreenController.java
@@ -1,7 +1,32 @@
 package team.nazah.customer;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
 public class PaymentScreenController
 {
     @javafx.fxml.FXML
+    private RadioButton paymentMethodCashRadioButton;
+    @javafx.fxml.FXML
+    private Label paymentScreenOrderIdLabel;
+    @javafx.fxml.FXML
+    private TextField cardNumberTextField;
+    @javafx.fxml.FXML
+    private Label paymentScreenTotalLabel;
+    @javafx.fxml.FXML
+    private TextField cardPinTextField;
+    @javafx.fxml.FXML
+    private Label paymentStatusLabel;
+    @javafx.fxml.FXML
+    private RadioButton paymentMethodCardRadioButton;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void payNowButtonOnAction(ActionEvent actionEvent) {
+    }
+}
