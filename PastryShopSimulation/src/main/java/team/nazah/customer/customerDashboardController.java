025c28910806cf3fa2894af0aece7f9ce44ad478
Nazah-projects-dump <nazahzahin2006@gmail.com>
2026-04-03 17:46:59 +0600

    customer dashboard fxml

--- PastryShopSimulation/src/main/java/team/nazah/customer/customerDashboardController.java
+++ PastryShopSimulation/src/main/java/team/nazah/customer/customerDashboardController.java
@@ -1,5 +1,6 @@
 package team.nazah.customer;
 
+import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
 
 public class CustomerDashboardController
@@ -9,4 +10,25 @@ public class CustomerDashboardController
 
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void trackOrderButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewCartButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logoutButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void feedbackButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewMenuButtonOnAction(ActionEvent actionEvent) {
+    }
+}
