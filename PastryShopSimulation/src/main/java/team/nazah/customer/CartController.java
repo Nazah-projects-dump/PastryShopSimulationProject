636c13c000bd21c2ac1f2868c1b4e13522fb2dfd
Nazah-projects-dump <nazahzahin2006@gmail.com>
2026-04-03 18:53:44 +0600

    cart fxml

--- PastryShopSimulation/src/main/java/team/nazah/customer/CartController.java
+++ PastryShopSimulation/src/main/java/team/nazah/customer/CartController.java
@@ -1,7 +1,32 @@
 package team.nazah.customer;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class CartController
 {
     @javafx.fxml.FXML
+    private TableColumn cartQuantityColumn;
+    @javafx.fxml.FXML
+    private TableView cartTableView;
+    @javafx.fxml.FXML
+    private TableColumn cartItemColumn;
+    @javafx.fxml.FXML
+    private TableColumn cartItemSubtotalColumn;
+    @javafx.fxml.FXML
+    private TableColumn cartPriceColumn;
+    @javafx.fxml.FXML
+    private Label cartTotalLabel;
+    @javafx.fxml.FXML
+    private TableColumn cartRemoveColumn;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void placeOrderButtonOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ PastryShopSimulation/src/main/java/team/nazah/customer/PaymentScreenController.java
@@ -0,0 +1,7 @@
+package team.nazah.customer;
+
+public class PaymentScreenController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
