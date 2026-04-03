732c89b993a4d07b7230aedcf529105df5662124
Nazah-projects-dump <nazahzahin2006@gmail.com>
2026-04-03 22:42:13 +0600

    incoming order details fxml

--- PastryShopSimulation/src/main/java/team/nazah/cashier/IncomingOrderDetailsController.java
+++ PastryShopSimulation/src/main/java/team/nazah/cashier/IncomingOrderDetailsController.java
@@ -1,7 +1,38 @@
 package team.nazah.cashier;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class IncomingOrderDetailsController
 {
     @javafx.fxml.FXML
+    private Label orderDetailsLabel;
+    @javafx.fxml.FXML
+    private TextField paymentAmountTextField;
+    @javafx.fxml.FXML
+    private TextField discountTextField;
+    @javafx.fxml.FXML
+    private ComboBox updateOrderStatusComboBox;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void finalizeOrderButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void applyDiscountButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void issueReceiptButtonOnAction(ActionEvent actionEvent) {
+    }
+}
