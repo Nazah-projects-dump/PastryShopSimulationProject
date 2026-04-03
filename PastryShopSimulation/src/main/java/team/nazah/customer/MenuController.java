c79c6b21c2e54e64e6c1e2dff724c1c6b7852c91
Nazah-projects-dump <nazahzahin2006@gmail.com>
2026-04-03 20:13:07 +0600

    menu page fxml

--- PastryShopSimulation/src/main/java/team/nazah/customer/MenuController.java
+++ PastryShopSimulation/src/main/java/team/nazah/customer/MenuController.java
@@ -1,7 +1,44 @@
 package team.nazah.customer;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class MenuController
 {
     @javafx.fxml.FXML
+    private TableColumn menuQuantityColumn;
+    @javafx.fxml.FXML
+    private TextField menuMinPriceFilterTextField;
+    @javafx.fxml.FXML
+    private ComboBox menuCategoryComboBox;
+    @javafx.fxml.FXML
+    private TableView menuTableView;
+    @javafx.fxml.FXML
+    private TableColumn menuPriceColumn;
+    @javafx.fxml.FXML
+    private CheckBox veganCheckBox;
+    @javafx.fxml.FXML
+    private TableColumn menuItemColumn;
+    @javafx.fxml.FXML
+    private TextField menuMaxPriceFilterTextField;
+    @javafx.fxml.FXML
+    private TableColumn menuAddToCartColumn;
+    @javafx.fxml.FXML
+    private Label filterStatusLabel;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void applyFiltersButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void resetFiltersButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewCartFromMenuButtonOnAction(ActionEvent actionEvent) {
+    }
+}
