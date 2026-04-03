cc9391c2c50365a7207fe29d1873a498a2c4667d
Nazah-projects-dump <nazahzahin2006@gmail.com>
2026-04-03 18:03:46 +0600

    cashier dashboard fxml

--- PastryShopSimulation/src/main/java/team/nazah/cashier/cashierDashboardController.java
+++ PastryShopSimulation/src/main/java/team/nazah/cashier/cashierDashboardController.java
@@ -1,12 +1,36 @@
 package team.nazah.cashier;
 
+import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 public class cashierDashboardController
 {
     @javafx.fxml.FXML
     private Label dashboardTestLabel;
+    @javafx.fxml.FXML
+    private TableView cancellationReqsTable;
+    @javafx.fxml.FXML
+    private TableView incomingOrdersTable;
+    @javafx.fxml.FXML
+    private TableColumn incomingOrdersColumn;
+    @javafx.fxml.FXML
+    private TableColumn cancellationReqsOpenColumn;
+    @javafx.fxml.FXML
+    private TableColumn cancellationReqsColumn;
+    @javafx.fxml.FXML
+    private TableColumn incomingOrdersOpenColumn;
 
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void logoutButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewSalesSummaryButtonOnAction(ActionEvent actionEvent) {
+    }
+}
