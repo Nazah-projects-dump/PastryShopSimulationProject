7e55f907b5d7a4871694cd5c3b37dfd3916ae597
Nazah-projects-dump <nazahzahin2006@gmail.com>
2026-04-03 19:39:14 +0600

    feedback page fxml

--- PastryShopSimulation/src/main/java/team/nazah/customer/FeedbackController.java
+++ PastryShopSimulation/src/main/java/team/nazah/customer/FeedbackController.java
@@ -1,7 +1,24 @@
 package team.nazah.customer;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class FeedbackController
 {
     @javafx.fxml.FXML
+    private TextField commentTextField;
+    @javafx.fxml.FXML
+    private Label feedbackStatusLabel;
+    @javafx.fxml.FXML
+    private ComboBox ratingComboBox;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void submitFeedbackButtonOnAction(ActionEvent actionEvent) {
+    }
+}
