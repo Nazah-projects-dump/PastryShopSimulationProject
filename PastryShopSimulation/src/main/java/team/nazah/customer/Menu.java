b3b904a6fe122837c7e9dadbaf5d6aa891f2b6bc
Nazah-projects-dump <nazahzahin2006@gmail.com>
2026-04-09 23:53:35 +0600

    menu methods

--- PastryShopSimulation/src/main/java/team/nazah/customer/Menu.java
+++ PastryShopSimulation/src/main/java/team/nazah/customer/Menu.java
@@ -29,6 +29,47 @@ public class Menu implements Serializable {
                 '}';
     }
 
+    public MenuItem getItem(String itemId) {
+        for (MenuItem item : menuItems) {
+            if (item.getItemId().equals(itemId))
+                return item;
+        }
+        return null;
+    }
+
+    public ArrayList<MenuItem> getItemsByCategory(String category) {
+        ArrayList<MenuItem> categorized = new ArrayList<>();
+        for (MenuItem item : menuItems) {
+            if (item.getCategory().equals(category)) {
+                categorized.add(item);
+            }
+        }
+        return categorized;
+    }
+
+    public ArrayList<MenuItem> applyFilters(Double minPrice, Double maxPrice, Boolean veganOnly) {
+        ArrayList<MenuItem> filtered = new ArrayList<>();
+
+        for (MenuItem item : menuItems) {
+            boolean passes = true;
+
+            if (minPrice != null && item.getPrice() < minPrice) {
+                passes = false;
+            }
+            if (maxPrice != null && item.getPrice() > maxPrice) {
+                passes = false;
+            }
+            if (veganOnly != null && veganOnly && !item.isVegan()) {
+                passes = false;
+            }
+
+            if (passes) {
+                filtered.add(item);
+            }
+        }
+        return filtered;
+    }
+
     public static void saveMenu(Menu menu) {
 
     }
