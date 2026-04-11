c75cbe791b5f348712f96965f549ddb74c9c1eb8
Nazah-projects-dump <nazahzahin2006@gmail.com>
2026-04-12 01:38:40 +0600

    cancellation req details controller logic

--- PastryShopSimulation/src/main/java/team/nazah/cashier/CancellationRequestDetailsController.java
+++ PastryShopSimulation/src/main/java/team/nazah/cashier/CancellationRequestDetailsController.java
@@ -4,29 +4,23 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import team.nazah.customer.Order;
+import team.nazah.customer.OrderStatus;
 
 import java.io.IOException;
 
 public class CancellationRequestDetailsController
 {
     @javafx.fxml.FXML
-    private Label totalLabel;
-    @javafx.fxml.FXML
     private Label refundAmountLabel;
-    @javafx.fxml.FXML
-    private Label orderIdlabel;
-    @javafx.fxml.FXML
-    private Label cancellationStatusLabel;
-    @javafx.fxml.FXML
-    private Label refundStatusLabel;
     @javafx.fxml.FXML
     private TextField reasonTextField;
     @javafx.fxml.FXML
-    private Label orderStatusLabel;
+    private Label orderDetailslabel;
 
     @javafx.fxml.FXML
     public void initialize() {
@@ -34,21 +28,18 @@ public class CancellationRequestDetailsController
 
     private Order order;
     private Cashier cashier;
-
     public void receiveOrder(Order order) {
         this.order = order;
-    }
+        orderDetailslabel.setText(
+                "Order ID: " + order.getOrderId() +
+                        "\nStatus: " + order.getStatus() +
+                        "\nTotal: " + order.getTotalAmount()
+        );
 
+        refundAmountLabel.setText("Refund Amount: " + order.getTotalAmount());
+    }
     public void receiveCashier(Cashier cashier) {
         this.cashier = cashier;
-    }
-
-    @javafx.fxml.FXML
-    public void refundButtonOnAction(ActionEvent actionEvent) {
-    }
-
-    @javafx.fxml.FXML
-    public void confirmCancellationButtonOnAction(ActionEvent actionEvent) {
     }
 
     @javafx.fxml.FXML
@@ -58,5 +49,28 @@ public class CancellationRequestDetailsController
         Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         stage.setScene(scene);
         stage.show();
+    }
+
+    @javafx.fxml.FXML
+    public void confirmButtonOnAction(ActionEvent actionEvent) {
+        order.updateStatus(OrderStatus.CANCELLED);
+
+        Order.saveOrder(order);
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setContentText("Cancellation approved and refund processed.");
+        alert.show();
+
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/nazah/CashierDashboard.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
